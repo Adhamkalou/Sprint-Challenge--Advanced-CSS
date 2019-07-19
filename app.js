a80e8fd3a4f75a3fa47ed38b08db213bf2931470
Adhamkalou <dodoadhamkalou@gmail.com>
2019-07-19 13:57:05 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+const btn = document.querySelector('.day-night-btn');
+const form = document.querySelector('.sign-up-form');
+const formBtn = document.querySelector('.btn');
+
+
+btn.addEventListener('click', () => {
+    if (body.className === 'day') {
+        body.classList.remove('day');
+        body.classList.add('night');
+        btn.style.color = 'white';
+    } else if (body.className === 'night') {
+        body.classList.remove('night');
+        body.classList.add('day');
+        btn.style.color = 'black';
+    }
+})
+
+formBtn.addEventListener('click', () => {
+    form.style.display = 'flex';
+    formBtn.style.display = 'none';
+})
